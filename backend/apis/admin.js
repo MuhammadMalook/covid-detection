d250d93b36484c45a32f6bcdd2b5746e15b746cf
MuhammadMalook <maharmohammadmalook@gmail.com>
2022-10-25 02:17:08 +0500

    new apis added

--- backend/apis/admin.js
+++ backend/apis/admin.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken')
 
 const passport = require('passport')
 require('./userAuth')(passport)
+const JWT_SECRET = process.env.JWT_SECRET
+
 
 
 
@@ -22,13 +24,13 @@ app.get('/check', passport.authenticate('adminAuth',{ session: false }), (req,re
 
 app.post('/login', async(req, res)=>{
     const {email, password, role} = req.body
+    console.log(req.body)
     if(!email || !password || !role)
         return res.status(401).json({success:false, msg:"please enter full data"})
 
-    if(role == "admin") {
-        connection.con.query(`SELECT * from admin where email='${email}'`,function (error, results, fields) {
+        connection.con.query(`SELECT * from ${role} where email='${email}'`,function (error, results, fields) {
             if (error) return res.status(400).json({error:error}) ;
-            console.log(results[0].password)
+           // console.log(results[0].password)
             if(results.length > 0){
                 if(results[0].password == password)
                 {
@@ -36,6 +38,7 @@ app.post('/login', async(req, res)=>{
                         algorithm:'HS256',
                         expiresIn:'1h'
                     })
+                    delete results[0].password
                     return res.status(200).json({success:true, data:results, message:"logged in", token})
                 }
                     
@@ -47,30 +50,30 @@ app.post('/login', async(req, res)=>{
             return res.status(200).json({msg:"incorrect email"})
             }
             }) 
-    }
-    else if(role=="faculty"){
-            connection.con.query(`SELECT * from admin where email='${email}'`,function (error, results, fields) {
-                if (error) return res.status(400).json({error:error}) ;
-                console.log(results[0].password)
-                if(results.length > 0){
-                    if(results[0].password == password)
-                    {
-                        const token =  jwt.sign({email},process.env.JWT_SECRET, {
-                            algorithm:'HS256',
-                            expiresIn:'1h'
-                        })
-                        return res.status(200).json({success:true, data:results, message:"logged in", token})
-                    }
+
+    // else if(role=="faculty"){
+    //         connection.con.query(`SELECT * from facullty where email='${email}'`,function (error, results, fields) {
+    //             if (error) return res.status(400).json({error:error}) ;
+    //             console.log(results[0].password)
+    //             if(results.length > 0){
+    //                 if(results[0].password == password)
+    //                 {
+    //                     const token =  jwt.sign({email},process.env.JWT_SECRET, {
+    //                         algorithm:'HS256',
+    //                         expiresIn:'1h'
+    //                     })
+    //                     return res.status(200).json({success:true, data:results, message:"logged in", token})
+    //                 }
                         
-                    else
-                        return res.status(200).json({msg:"incorrect password"})
+    //                 else
+    //                     return res.status(200).json({msg:"incorrect password"})
                         
-                }
-                else{
-                return res.status(200).json({msg:"incorrect email"})
-                }
-                }) 
-    }
+    //             }
+    //             else{
+    //             return res.status(200).json({msg:"incorrect email"})
+    //             }
+    //             }) 
+    //}
 
 
 
@@ -79,7 +82,7 @@ app.post('/login', async(req, res)=>{
 app.get('/student',passport.authenticate('adminAuth',{ session: false }) ,async(req, res)=>{
     
     console.log("after verify")
-    connection.con.query("SELECT * from student", function(error, results, fields){
+    connection.con.query("SELECT name, email, college, TAGID from student", function(error, results, fields){
         if(error) return res.status(400).json({success:false,message:error})
         if(results.length > 0)
         return res.status(200).json({success:true, data:results})
@@ -92,7 +95,7 @@ app.get('/student',passport.authenticate('adminAuth',{ session: false }) ,async(
   app.get('/faculty',passport.authenticate('adminAuth',{ session: false }) ,async(req, res)=>{
     
     console.log("after verify")
-    connection.con.query("SELECT * from faculty", function(error, results, fields){
+    connection.con.query("SELECT name, email, college, TAGID from faculty", function(error, results, fields){
         if(error) return res.status(400).json({success:false,message:error})
         if(results.length > 0)
         return res.status(200).json({success:true, data:results})
@@ -146,7 +149,7 @@ app.get('/student',passport.authenticate('adminAuth',{ session: false }) ,async(
 app.get('/students-temp',passport.authenticate('adminAuth',{ session: false }) ,async(req, res)=>{
     
     console.log("after verify")
-    connection.con.query("SELECT * from student join temperature using(TAGID)", function(error, results, fields){
+    connection.con.query("SELECT name, email, college, TAGID, BodyTemp  from student join temperature using(TAGID)", function(error, results, fields){
         if(error) return res.status(400).json({success:false,message:error})
         if(results.length > 0)
         return res.status(200).json({success:true, data:results})
@@ -159,7 +162,7 @@ app.get('/students-temp',passport.authenticate('adminAuth',{ session: false }) ,
   app.get('/faculty-temp',passport.authenticate('adminAuth',{ session: false }) ,async(req, res)=>{
     
     console.log("after verify")
-    connection.con.query("SELECT * from faculty join temperature using(TAGID)", function(error, results, fields){
+    connection.con.query("SELECT name, email, college, TAGID, BodyTemp  from faculty join temperature using(TAGID)", function(error, results, fields){
         if(error) return res.status(400).json({success:false,message:error})
         if(results.length > 0)
         return res.status(200).json({success:true, data:results})
@@ -168,6 +171,53 @@ app.get('/students-temp',passport.authenticate('adminAuth',{ session: false }) ,
 
     })
   })  
+
+
+  app.get('/checkAdmin/:token', async(req, res)=>{
+    const {token} = req.params
+    console.log("params", req.params)
+    try{
+        payload = jwt.verify(JSON.parse(token), JWT_SECRET)
+        console.log(payload.email, "sdmfsdfs")
+        connection.con.query(`SELECT email from admin where email='${payload.email}'`, function(error, results, fields){
+            if(error) return res.status(400).json({success:false,message:error})
+            if(results.length > 0)
+            return res.status(200).json({success:true, data:results, role:"admin"})
+            else
+            {
+                connection.con.query(`SELECT email from student where email='${payload.email}'`, function(error, results, fields){
+                    if(error) return res.status(400).json({success:false,message:error})
+                    if(results.length > 0)
+                    return res.status(200).json({success:true, role:"student"})
+                    else
+                    {
+                        connection.con.query(`SELECT email from faculty where email='${payload.email}'`, function(error, results, fields){
+                            if(error) return res.status(400).json({success:false,message:error})
+                            if(results.length > 0)
+                            return res.status(200).json({success:true, role:"faculty"})
+                            else
+                            {
+                                return res.status(200).json({success:false, msg:"no record found"})
+                            }
+                        })
+                    }
+                })
+            }
+        })
+
+    }
+    catch(e){
+        if (e instanceof jwt.JsonWebTokenError) {
+            // if the error thrown is because the JWT is unauthorized, return a 401 error
+            return res.status(401).json({success:false, msg:e})
+          }
+          // otherwise, return a bad request error
+          return res.status(400).json({success:false, msg:'wrong token'})
+    }
+   
+    //console.log(payload)
+   
+  })
 
 
 module.exports = app
