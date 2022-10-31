50970c0ef8ab5696146e8103064021980aacae2d
MuhammadMalook <maharmohammadmalook@gmail.com>
2022-10-31 21:22:24 +0500

    accept button functionality added with add pcr api

--- frontend/src/screens/Requests.js
+++ frontend/src/screens/Requests.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
-
+import { json, useNavigate } from 'react-router-dom';
 import BounceLoader from 'react-spinners/BounceLoader';
 
 export default function Requests() {
 
     const [data, setData] = useState([])
     const [loading, isLoading] = useState(false)
+    const navigate = useNavigate()
 
 
 
     const getRequests = async() => {
+    
         isLoading(true)
         const response = await fetch('/requests', {
             method:"GET"
@@ -49,7 +51,7 @@ export default function Requests() {
               }}  size={60} aria-label="Loading Spinner" />
         }
 
-   { !loading && data.length > 0 ? <div><table class="table table-image" style={{verticalAlign:'middle', textAlign:'center'}}>
+   { !loading && data.length > 0 ? <div style={{overflow:'auto'}}><table class="table table-image" style={{verticalAlign:'middle', textAlign:'center',  overflow:'auto', }}>
     <thead>
       <tr>
         <th scope="col">Image</th>
@@ -66,13 +68,56 @@ export default function Requests() {
                 return <tr>
      
                 <td class="w-25">
-                    <img src={`/uploads/${item.imageSrc}`} class="img-fluid img-thumbnail" alt="pcr"/>
+                    <img src={`/uploads/${item.imageSrc}`} class="img-fluid img-thumbnail " alt="pcr" style={{height:300, width:400}}/>
                 </td>
                 <td>{item.TAGID}</td>
                 <td>{item.BodyTemp}</td>
                 <td>{item.Test_Status}</td>
 
-                <td><button className="btn btn-success" type="button">Accept</button></td>
+                <td><button className="btn btn-success" type="button" onClick={async()=>{
+                    var token =  localStorage.getItem('token')
+                    const body = {TAGID: item.TAGID, pcrResult:item.pcrResult, date:item.Test_Date, image:item.imageSrc}
+                    console.log(body)
+                    if(token)
+                    {
+                        console.log(token)
+                        var auth = "Bearer ".concat(JSON.parse(token))
+                        console.log(auth)
+                        const response = await fetch('/add-pcr', {
+                        method:"POST",
+                        headers:{
+                            "Content-Type":"application/json",
+                            "Authorization": auth,
+
+                        },
+                        body: JSON.stringify(body)
+                    })
+                    if(response.statusText == "Unauthorized")
+                    {
+                        console.log("heeereree")
+                        localStorage.clear()
+                        navigate('/')
+                    }
+                    else{
+                        const jsonData = await response.json()
+                        console.log(jsonData)
+                        console.log(jsonData,"jsonDAta")
+                        //setApi(false)
+                        if(jsonData.success)
+                        {
+                           alert("Accepted")
+                        }
+                        else {
+                            localStorage.clear()
+                            navigate('/')
+                    
+                        }
+                       }
+                }
+                else{
+                    alert("error")
+                }
+                }}>Accept</button></td>
                 <td><button className="btn btn-warning" type="Reject">Reject</button></td>
         
               </tr>
